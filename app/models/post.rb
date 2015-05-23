5d4a1ae90e780ec7f31ebf27863c0fbd33c3954a
Jesse House <mail@jessehouse.com>
2015-05-23 12:28:10 -0700

    add callback to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+  after_save :some_complex_logic
+
+  private
+
+  def some_complex_logic
+    # complex logic around published_at
+    # - publish rss, tweet, etc...
+  end
 end
